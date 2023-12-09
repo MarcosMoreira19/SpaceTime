fb8c830e6d5c74e993ebbad9bdd6b1310e4d1743
Marcos Vinícius <marcosmoreirasantos2002@gmail.com>
2023-12-09 14:25:15 -0300

    Commit inicial

---
+++ NLW-Spacetime-Rocketseat-2023-main/server/src/server.ts
@@ -0,0 +1,42 @@
+import 'dotenv/config';
+
+import fastify from 'fastify';
+import jwt from '@fastify/jwt';
+import cors from '@fastify/cors';
+import multipart from '@fastify/multipart';
+
+import { memoriesRoutes } from './routes/memories';
+import { authRoutes } from './routes/auth';
+import { uploadRoutes } from './routes/upload';
+import { resolve } from 'node:path';
+
+const app = fastify();
+
+app.register(multipart);
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+});
+
+app.register(cors, {
+  origin: true,
+});
+
+app.register(jwt, {
+  secret: 'spacetime',
+});
+
+app.register(memoriesRoutes);
+app.register(authRoutes);
+app.register(uploadRoutes);
+
+app
+  .listen({
+    port: 3333,
+    host: '0.0.0.0',
+  })
+  .then(() => {
+    console.log('HTTP server running on http://localhost:3333');
+  });
---
+++ NLW-Spacetime-Rocketseat-2023-main/web/next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      'avatars.githubusercontent.com',
+      '127.0.0.1',
+      '192.168.1.9',
+      '192.168.1.11',
+    ],
+  },
+};
+
+module.exports = nextConfig;
